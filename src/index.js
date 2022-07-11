4cd0b836757d3ed45779b81034c6d52fadbedb67
Tainã Martins Pino <63248706+Tainmat@users.noreply.github.com>
2022-07-11 11:05:53 -0300

    Create POST /users

--- src/index.js
+++ src/index.js
@@ -1,40 +1,55 @@
-const express = require('express');
-const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
-
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { id } = request.headers;
 }
 
-app.post('/users', (request, response) => {
+app.post("/users", (request, response) => {
+  const { name, username } = request.body;
+
+  usernameExists = users.some((user) => user.username === username);
+
+  if (usernameExists) {
+    return response
+      .status(400)
+      .json({ err: `Username '${username}' already exists` });
+  }
+
+  users.push({
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todo: [],
+  });
+
+  return response.status(201).send(users[users.length - 1].id);
+});
+
+app.get("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.post("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
